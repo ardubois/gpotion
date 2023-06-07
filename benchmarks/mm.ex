8a622f2b3859b6f592ebcca45005a229a4c0094a
ardubois <rauberdubois@gmail.com>
2023-06-07 19:44:49 -0300

    ready for bench

--- benchmarks/mm.ex
+++ benchmarks/mm.ex
@@ -16,10 +16,11 @@ gpotion mm(a,b,c,m,n,k) do
 end
 end
 
-m = 1000
-n = 1000
-k = 1000
+[arg] = System.argv()
 
+m = String.to_integer(arg)
+n = m
+k=m
 
 mat = Matrex.fill(1,m*k,1)
 
@@ -38,17 +39,18 @@ prev = System.monotonic_time()
 
 a=GPotion.new_gmatrex(mat1)
 b=GPotion.new_gmatrex(mat2)
-c=GPotion.new_gmatrex(m*k)
+c=GPotion.new_gmatrex(1,m*k)
 
 GPotion.spawn(ker,{grid_rows,grid_cols,1},{block_size,block_size,1},[a,b,c,m,n,k])
 GPotion.synchronize()
 
-result = GPotion.get_matrex(c)
+result = GPotion.get_gmatrex(c)
 
 next = System.monotonic_time()
-IO.puts "time gpu #{System.convert_time_unit(next-prev,:native,:millisecond)}"
+#IO.puts "time gpu #{System.convert_time_unit(next-prev,:native,:millisecond)}"
+IO.puts "gpu  #{m}  #{System.convert_time_unit(next-prev,:native,:millisecond)} "
 
-IO.inspect result
+#IO.inspect result
 #IO.puts GPU.Backend.gen_c_kernel('addVectors',4,[])
 
 amat = Matrex.reshape(mat1,m,k)
@@ -57,7 +59,8 @@ bmat = Matrex.reshape(mat2,m,k)
 prev = System.monotonic_time()
 cmat = Matrex.dot(amat,bmat)
 next = System.monotonic_time()
-IO.puts "time cpu #{System.convert_time_unit(next-prev,:native,:millisecond)}"
+#IO.puts "time cpu #{System.convert_time_unit(next-prev,:native,:millisecond)}"
+IO.puts "cpu  #{m}  #{System.convert_time_unit(next-prev,:native,:millisecond)} "
 
 rmat = Matrex.reshape(result,m,k)
 
