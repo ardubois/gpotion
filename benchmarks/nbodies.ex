c427fe8e8ef76ce523c2bfd8617a71cfb6ad3eca
ardubois <rauberdubois@gmail.com>
2023-06-11 22:38:27 -0300

    fixed recursion

--- benchmarks/nbodies.ex
+++ benchmarks/nbodies.ex
@@ -29,13 +29,13 @@ defmodule NBodies do
     p
   end
   def nbodies(i,p,dt,softening,n) do
-    p=nbodies(i-1,p,dt,softening,n)
+    #p=nbodies(i-1,p,dt,softening,n)
     {fx,fy,fz} = calc_nbodies(n,i,p,softening,0.0,0.0,0.0)
 
     p=Matrex.set(p,1,6*i+4,Matrex.at(p,1,6*i+4)+ dt*fx);
     p=Matrex.set(p,1,6*i+5,Matrex.at(p,1,6*i+5) + dt*fy);
-    Matrex.set(p,1,6*i+6,Matrex.at(p,1,6*i+6) + dt*fz);
-
+    p=Matrex.set(p,1,6*i+6,Matrex.at(p,1,6*i+6) + dt*fz);
+    nbodies(i-1,p,dt,softening,n)
   end
 
 def calc_nbodies(-1,_i,_p,_softening,fx,fy,fz) do
