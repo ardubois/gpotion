16675bc5a00af72cc11015af2c362383f1198fa9
ardubois <rauberdubois@gmail.com>
2023-06-14 13:25:16 -0300

    new sequential version

--- benchmarks/ripple_seq.ex
+++ benchmarks/ripple_seq.ex
@@ -12,36 +12,42 @@ defmodule BMP do
 end
 
 defmodule Ripple do
-
-  def gen_pixel(dim,ticks,{y,x}) do
+  def gen_pixel(ptr,dim,ticks,{y,x}) do
+    offset = x + (y * dim)
     fx = 0.5 *  x - dim/15;#x - dim/2;
     fy = 0.5 *  y - dim/15;#y - dim/2;
     d  = :math.sqrt( fx * fx + fy * fy );
     grey = floor(128.0 + 127.0 * :math.cos(d/10.0 - ticks/7.0) /(d/10.0 + 1.0));
-    [grey,grey,grey,255]
 
+    ptr
+    |> Matrex.set(1, offset*4 + 1, grey)
+    |> Matrex.set(1, offset*4 + 2, grey)
+    |> Matrex.set(1, offset*4 + 3, grey)
+    |> Matrex.set(1, offset*4 + 4, 255)
   end
-  def ripple_seq(dim, ticks, [{y,x}]) do
-    gen_pixel(dim, ticks, {y,x})
+  def ripple_seq(ptr,dim, ticks, [{y,x}]) do
+    gen_pixel(ptr,dim, ticks, {y,x})
   end
-  def ripple_seq( dim,ticks ,[{y,x}|tail]) do
-    l=ripple_seq(dim,ticks, tail)
-    narray = gen_pixel(dim,ticks, {y,x})
-    narray ++ l
+  def ripple_seq(ptr, dim,ticks ,[{y,x}|tail]) do
+    narray = gen_pixel(ptr,dim,ticks, {y,x})
+    ripple_seq(narray,dim,ticks, tail)
   end
 end
 
 [arg] = System.argv()
 
-usr_value = String.to_integer(arg)
+user_value = String.to_integer(arg)
 
-dim = usr_value
+dim = user_value
+
+mat = Matrex.fill(1,dim*dim*4,0)
+
 
 indices = for i <- Enum.to_list(0..(dim-1)), j<-Enum.to_list(0..(dim-1)), do: {i,j}
-#IO.inspect indices
+
 prev = System.monotonic_time()
-imagelist= Ripple.ripple_seq(dim,10,indices)
+imageseq = Ripple.ripple_seq(mat,dim,10,indices)
 next = System.monotonic_time()
-IO.puts "time cpu #{System.convert_time_unit(next-prev,:native,:millisecond)}"
-imageseq=Matrex.new([imagelist])
+IO.puts "Elixir\t#{user_value}\t#{System.convert_time_unit(next-prev,:native,:millisecond)}"
+
 BMP.gen_bmp('rippleseq.bmp',dim,imageseq)
