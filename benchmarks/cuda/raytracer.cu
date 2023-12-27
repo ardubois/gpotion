93b07856e400786d31dfea854ff5f8d051936fc6
Andre Rauber Du Bois <rauberdubois@gmail.com>
2023-12-26 21:42:51 -0300

    image

--- benchmarks/cuda/raytracer.cu
+++ benchmarks/cuda/raytracer.cu
@@ -1,107 +1,79 @@
 #include <stdio.h>
 #include <time.h>
-
+#include <stdint.h>
 #define rnd( x ) (x * rand() / RAND_MAX)
 #define INF     2e10f
 
-const int BYTES_PER_PIXEL = 4; /// red, green, blue & 255
-const int FILE_HEADER_SIZE = 14;
-const int INFO_HEADER_SIZE = 40;
+#define _bitsperpixel 32
+#define _planes 1
+#define _compression 0
 
-void generateBitmapImage(unsigned char* image, int height, int width, char* imageFileName);
-unsigned char* createBitmapFileHeader(int height, int stride);
-unsigned char* createBitmapInfoHeader(int height, int width);
+#define _xpixelpermeter 0x13B //0x130B //2835 , 72 DPI
+#define _ypixelpermeter 0x13B//0x130B //2835 , 72 DPI
+#define pixel 0xFF
+#pragma pack(push,1)
+typedef struct{
+    uint8_t signature[2];
+    uint32_t filesize;
+    uint32_t reserved;
+    uint32_t fileoffset_to_pixelarray;
+} fileheader;
+typedef struct{
+    uint32_t dibheadersize;
+    uint32_t width;
+    uint32_t height;
+    uint16_t planes;
+    uint16_t bitsperpixel;
+    uint32_t compression;
+    uint32_t imagesize;
+    uint32_t ypixelpermeter;
+    uint32_t xpixelpermeter;
+    uint32_t numcolorspallette;
+    uint32_t mostimpcolor;
+} bitmapinfoheader;
+typedef struct {
+    fileheader fileheader;
+    bitmapinfoheader bitmapinfoheader;
+} bitmap;
+#pragma pack(pop)
+void genBpm (int height, int width, float *pixelbuffer_f) {
+    uint32_t pixelbytesize = height*width*_bitsperpixel/8;
+    uint32_t  _filesize =pixelbytesize+sizeof(bitmap);
+    FILE *fp = fopen("test.bmp","wb");
+    bitmap *pbitmap  = (bitmap*)calloc(1,sizeof(bitmap));
 
+    int buffer_size = height*width*4;
+    uint8_t *pixelbuffer = (uint8_t*)malloc(buffer_size);
 
-void generateLog(double time, int dim, int spheres, int iteration){
-  printf("Writting operation time to file.\n");
-  FILE *file;
-  char filename[] = "time-c-gpuraytracer.txt";
-
-  file = fopen(filename, "a");
-  fprintf(file, "time: %f, \t iteration: %d, \t dimension: %d, \t spheres %d\n", time, iteration, dim, spheres); 
-  fclose(file);
-  
-  printf("done.\n");
-
-
-}
-
-void generateBitmapImage (unsigned char* image, int height, int width, char* imageFileName){
-    int widthInBytes = width * BYTES_PER_PIXEL;
-
-    unsigned char padding[3] = {0, 0, 0};
-    int paddingSize = (4 - (widthInBytes) % 4) % 4;
-
-    int stride = (widthInBytes) + paddingSize;
-
-    FILE* imageFile = fopen(imageFileName, "wb");
-
-    unsigned char* fileHeader = createBitmapFileHeader(height, stride);
-    fwrite(fileHeader, 1, FILE_HEADER_SIZE, imageFile);
-
-    unsigned char* infoHeader = createBitmapInfoHeader(height, width);
-    fwrite(infoHeader, 1, INFO_HEADER_SIZE, imageFile);
-
-    int i;
-    for (i = 0; i < height; i++) {
-        fwrite(image + (i*widthInBytes), BYTES_PER_PIXEL, width, imageFile);
-        fwrite(padding, 1, paddingSize, imageFile);
+    for(int i = 0; i<buffer_size;i++)
+    {
+     pixelbuffer[i]= (uint8_t) pixelbuffer_f[i];
     }
 
-    fclose(imageFile);
+
+    //strcpy(pbitmap->fileheader.signature,"BM");
+    pbitmap->fileheader.signature[0] = 'B';
+    pbitmap->fileheader.signature[1] = 'M';
+    pbitmap->fileheader.filesize = _filesize;
+    pbitmap->fileheader.fileoffset_to_pixelarray = sizeof(bitmap);
+    pbitmap->bitmapinfoheader.dibheadersize =sizeof(bitmapinfoheader);
+    pbitmap->bitmapinfoheader.width = width;
+    pbitmap->bitmapinfoheader.height = height;
+    pbitmap->bitmapinfoheader.planes = _planes;
+    pbitmap->bitmapinfoheader.bitsperpixel = _bitsperpixel;
+    pbitmap->bitmapinfoheader.compression = _compression;
+    pbitmap->bitmapinfoheader.imagesize = pixelbytesize;
+    pbitmap->bitmapinfoheader.ypixelpermeter = _ypixelpermeter ;
+    pbitmap->bitmapinfoheader.xpixelpermeter = _xpixelpermeter ;
+    pbitmap->bitmapinfoheader.numcolorspallette = 0;
+    fwrite (pbitmap, 1, sizeof(bitmap),fp);
+    //memset(pixelbuffer,pixel,pixelbytesize);
+    fwrite(pixelbuffer,1,pixelbytesize,fp);
+    fclose(fp);
+    free(pbitmap);
+    free(pixelbuffer);
 }
 
-unsigned char* createBitmapFileHeader (int height, int stride){
-    int fileSize = FILE_HEADER_SIZE + INFO_HEADER_SIZE + (stride * height);
-
-    static unsigned char fileHeader[] = {
-        0,0,     /// signature
-        0,0,0,0, /// image file size in bytes
-        0,0,0,0, /// reserved
-        0,0,0,0, /// start of pixel array
-    };
-
-    fileHeader[ 0] = (unsigned char)('B');
-    fileHeader[ 1] = (unsigned char)('M');
-    fileHeader[ 2] = (unsigned char)(fileSize      );
-    fileHeader[ 3] = (unsigned char)(fileSize >>  8);
-    fileHeader[ 4] = (unsigned char)(fileSize >> 16);
-    fileHeader[ 5] = (unsigned char)(fileSize >> 24);
-    fileHeader[10] = (unsigned char)(FILE_HEADER_SIZE + INFO_HEADER_SIZE);
-
-    return fileHeader;
-}
-
-unsigned char* createBitmapInfoHeader (int height, int width){
-    static unsigned char infoHeader[] = {
-        0,0,0,0, /// header size
-        0,0,0,0, /// image width
-        0,0,0,0, /// image height
-        0,0,     /// number of color planes
-        0,0,     /// bits per pixel
-        0,0,0,0, /// compression
-        0,0,0,0, /// image size
-        0,0,0,0, /// horizontal resolution
-        0,0,0,0, /// vertical resolution
-        0,0,0,0, /// colors in color table
-        0,0,0,0, /// important color count
-    };
-
-    infoHeader[ 0] = (unsigned char)(INFO_HEADER_SIZE);
-    infoHeader[ 4] = (unsigned char)(width      );
-    infoHeader[ 5] = (unsigned char)(width >>  8);
-    infoHeader[ 6] = (unsigned char)(width >> 16);
-    infoHeader[ 7] = (unsigned char)(width >> 24);
-    infoHeader[ 8] = (unsigned char)(height      );
-    infoHeader[ 9] = (unsigned char)(height >>  8);
-    infoHeader[10] = (unsigned char)(height >> 16);
-    infoHeader[11] = (unsigned char)(height >> 24);
-    infoHeader[12] = (unsigned char)(1);
-    infoHeader[14] = (unsigned char)(BYTES_PER_PIXEL*8);
-
-    return infoHeader;
-}
 
 struct Sphere {
     float   r,b,g;
@@ -280,8 +252,8 @@ temp_s[19]=	 { 0.5646229438154241	 ,0.6811426129947813	 , 0.023316141239661855	 
     cudaEventElapsedTime(&time, start, stop) ;
 
      printf("CUDA\t%d\t%3.1f\n", dim,time);
-
-    
+     genBpm(dim,dim,final_image);
+    /*
     int height = dim;
     int width = dim;
     
@@ -312,6 +284,6 @@ temp_s[19]=	 { 0.5646229438154241	 ,0.6811426129947813	 , 0.023316141239661855	 
     free(image);
     free(temp_s);
     free(final_image);
-
+*/
 
 }
